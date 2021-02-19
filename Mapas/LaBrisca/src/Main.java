e77fa33e4539e97c337f49dc6771c82a1865a3b9
SergioAnaya <57174411+SergioAM99@users.noreply.github.com>
2021-02-19 16:07:51 +0100

    Cambios

--- Mapas/LaBrisca/src/Main.java
+++ Mapas/LaBrisca/src/Main.java
@@ -1,48 +1,12 @@
-import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.ObjectOutputStream;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
-
 public class Main {
 
-    public static void main(String[] args) throws Exception { // RECREAR CON OPENCSV
+    public static void main(String[] args) throws Exception {
 
-        Scanner teclado = new Scanner(System.in);
+        Campeonato campeonato = new Campeonato();
 
-        Persona persona;
+        campeonato.leerArchivo("Jugadores.csv");
 
-        ObjectOutputStream fichero = new ObjectOutputStream(new FileOutputStream("Jugadores.csv"));
-        Archivo archivo = new Archivo();
-
-        System.out.print("Introduce la cantidad de jugadores: ");
-        int numJugadores = teclado.nextInt();
-
-        for (int i = 0; i <= numJugadores; i++) {
-            System.out.print("NickName: ");
-            String nickName = teclado.next();
-            System.out.print("Nombre: ");
-            String nombre = teclado.next();
-            System.out.print("Apellidos: ");
-            String apellidos = teclado.next();
-            System.out.print("Edad: ");
-            int edad = teclado.nextInt();
-
-            persona = new Persona(nickName, nombre, apellidos, edad);
-
-            System.out.println("hola");
-
-            fichero.writeObject(persona);
-        }
-        fichero.close();
-
-
-        List<String []> filas = archivo.leerArchivo("Jugadores.csv");
-
-        for (String [] campos : filas) {
-            System.out.println(Arrays.toString(campos));
-        }
+        System.out.println(campeonato.partidas.get("Jesfly").toString());
 
     }
 
